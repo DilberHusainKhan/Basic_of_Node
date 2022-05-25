88b4baf4a1b232db3b517b01a8b3e444a7c14859
dilberhusainkhan <dilberjmi@gmail.com>
2022-05-25 14:25:49 +0530

    Hoisting in js

---
+++ 2_Function/2_Hoisting.js
@@ -0,0 +1,17 @@
+console.log("Script started");
+
+// function invocation 
+iamReal();
+
+// 1. function defination
+function iamReal(){
+    console.log("I am Real");
+} 
+
+// 2. function defination
+function iamReal(){
+    console.log("He is not real, I am the real one");
+}
+
+// function invocation
+iamReal();
