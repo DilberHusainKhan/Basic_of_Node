7e5d8373c06629fde1a45feb11899fd3aa145c7e
dilberhusainkhan <dilberjmi@gmail.com>
2022-05-25 14:34:21 +0530

    add some theory

--- 2_Function/2_Hoisting.js
+++ 2_Function/2_Hoisting.js
@@ -1,15 +1,19 @@
+// memory for the  function are allocated before code is executed
+// function are created in heep and there addresses are stored in stack
+// there is no function overloading in js.
+
 console.log("Script started");
 
 // function invocation 
 iamReal();
 
 // 1. function defination
-function iamReal(){
+function iamReal() {
     console.log("I am Real");
-} 
+}
 
 // 2. function defination
-function iamReal(){
+function iamReal() {
     console.log("He is not real, I am the real one");
 }
 
