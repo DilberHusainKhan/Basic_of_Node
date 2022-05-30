fe797cf282781c827121cc76dcae3488da891ab8
dilberhusainkhan <dilberjmi@gmail.com>
2022-05-30 20:08:55 +0530

    question on closure

---
+++ 2_Function/11_Ques1.js
@@ -0,0 +1,30 @@
+function outer() {
+    var arr = [];
+    for (let i = 0; i < 3; i++) {
+        arr.push(function () {
+            console.log(i);
+        })
+    }
+    return arr;
+}
+console.log("Before calling outer");
+var arr = outer();
+arr[0]();
+arr[1]();
+arr[2]();
+console.log("After calling outer");
+
+// if we use var then we get 3,3,3 as answer
+// Before calling outer
+// 3
+// 3
+// 3
+// After calling outer
+
+
+// if we use let then we get 0,1,2
+// Before calling outer
+// 0
+// 1
+// 2
+// After calling outer
