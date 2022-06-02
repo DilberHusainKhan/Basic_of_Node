bf9a48970f0ff575ece8ac3d314c62b91b78a8ad
dilberhusainkhan <dilberjmi@gmail.com>
2022-06-02 14:11:21 +0530

    custom bind

--- 2_Function/14_2_MyBind.js
+++ 2_Function/14_2_MyBind.js
@@ -14,3 +14,36 @@ let obj2 = {
     fullName: "Humayun Anwar",
     age: 23
 };
+
+// bind
+let fun = obj.fun1.bind(obj2, "BBBB", "CCCCC", "DDDDDDD")
+fun();
+
+// custom bind
+Function.prototype.myBind = function (func, ...args) {
+    let self = this;
+    return function () {
+        return self.apply(func, [...args]);
+    }
+}
+let fun2 = obj.fun1.myBind(obj2, "BBBB", "CCCCC", "DDDDDDD")
+fun2();
+
+// custom bind by pepcoding
+Function.prototype.myBind1 = function () {
+    let orgfun = this;
+    let args = Array.from(arguments);   //change arguments to array and store in args
+
+    let boundFunction = function () {
+        let thisForOrgFun = args[0];    //I st parameter is function
+        let argsForOrgFun = args.slice(1);  //argument are start from 1 to last
+        let argFroInvocation = Array.from(arguments);   //parameter provide when function call
+        argsForOrgFun = argsForOrgFun.concat(argFroInvocation);
+        orgfun.apply(thisForOrgFun, argsForOrgFun);
+    }
+    return boundFunction;
+}
+
+let newBindResult = obj.fun1.myBind1(obj2, "JAZZ", "JJJJJ", "KKKKK", "LLLL");
+newBindResult("MMMM");
+
